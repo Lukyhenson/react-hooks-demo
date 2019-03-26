e6593da52da101b43bd7036e0d99f2e5f8aeff9f
Tauan Tathiell <lukyhenson@live.com>
2019-03-26 15:29:22 -0300

    It creates the class component example

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,11 +1,15 @@
+import '../styles/components/row.scss'
+
 import React from 'react'
 import PropTypes from 'prop-types'
+
+const componetClassName = 'row';
 
 function Row(props) {
   return (
     <React.Fragment>
-      <h1>{props.label}</h1>
-      <div>
+      <h1 className={`${componetClassName}__label`}>{props.label}</h1>
+      <div className={`${componetClassName}__children`}>
         {props.children}
       </div>
     </React.Fragment>
